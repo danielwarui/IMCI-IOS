8ecba067d34c97ba71dfad3f5ce804ae46b3c263
danielwarui <waruindungu@gmail.com>
2016-01-20 02:01:28 +0300

    first commit ... Assess classify tabs pending. Counsel the mother three quarters done Treat the child half done

---
+++ IMCI/ContentViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ContentViewController.swift
+//  IMCI
+//
+//  Created by waruindungu on 18/01/2016.
+//  Copyright © 2016 Ministry of Health. All rights reserved.
+//
+
+import UIKit
+
+class ContentViewController: UIViewController {
+
+}
