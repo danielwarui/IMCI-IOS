3cebcdb4767165998c988d4a42e9d335888af0a5
danielwarui <waruindungu@gmail.com>
2016-03-01 12:42:35 +0300

    75 % complete Wonderful proceeds on scrollViews and Styling views Perfect image Processing with double tap to zoom or pinch gesture

---
+++ IMCI/TreatChild_Wheezing.swift
@@ -0,0 +1,69 @@
+//
+//  TreatChild_Wheezing.swift
+//  IMCI
+//
+//  Created by waruindungu on 01/03/2016.
+//  Copyright © 2016 Ministry of Health. All rights reserved.
+
+import UIKit
+
+class TreatChild_Wheezing: UIViewController, UIScrollViewDelegate {
+    var scrollView: UIScrollView!
+    var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imageView = UIImageView(image: UIImage(named: "treat_wheezing.png"))
+        
+        scrollView = UIScrollView(frame: view.bounds)
+        // scrollView.backgroundColor = UIColor.brownColor()
+        scrollView.contentSize = imageView.bounds.size
+        // scrollView.autoresizingMask = UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleHeight
+        // scrollView.contentOffset = CGPoint(x: 1000, y: 450)
+        
+        scrollView.delegate = self
+        // scrollView.minimumZoomScale = 0.1
+        // scrollView.maximumZoomScale = 4.0
+        // scrollView.zoomScale = 0.3
+        
+        setZoomScale()
+        setupGestureRecognizer()
+        
+        scrollView.addSubview(imageView)
+        view.addSubview(scrollView)
+        
+        
+    }
+    
+    func setupGestureRecognizer() {
+        let doubleTap = UITapGestureRecognizer(target: self, action: "handleDoubleTap:")
+        doubleTap.numberOfTapsRequired = 2
+        scrollView.addGestureRecognizer(doubleTap)
+    }
+    
+    func handleDoubleTap(recognizer: UITapGestureRecognizer) {
+        
+        if (scrollView.zoomScale > scrollView.minimumZoomScale) {
+            scrollView.setZoomScale(scrollView.minimumZoomScale, animated: true)
+        } else {
+            scrollView.setZoomScale(scrollView.maximumZoomScale, animated: true)
+        }
+    }
+    func setZoomScale() {
+        let imageViewSize = imageView.bounds.size
+        let scrollViewSize = scrollView.bounds.size
+        let widthScale = scrollViewSize.width / imageViewSize.width
+        let heightScale = scrollViewSize.height / imageViewSize.height
+        
+        scrollView.minimumZoomScale = min(widthScale, heightScale)
+        scrollView.zoomScale = 1.0
+    }
+    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    override func viewWillLayoutSubviews() {
+        setZoomScale()
+    }
+}
+
